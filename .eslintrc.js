a4b739c8a19b1fb2e71889fffd39176bedd97623
Merlin Beutlberger <r@ndombyte.net>
2020-12-30 03:11:56 +0100

    Fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "es6": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 8
-    },
-    "extends": ["eslint:recommended", "google"],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-negated-condition": "off",
-        "require-jsdoc": "off",
-        "no-mixed-requires": "off",
-        "max-len": ["warn", 120],
-        "no-implicit-coercion": [
-            2,
-            { "allow": ["!!"] }
-        ],
-        "comma-dangle": "off",
-        "no-var": "off",
-        "no-tabs": "off",
-        "no-console": "off"
-    },
-    "root": true
+	"env": {
+		"node": true,
+		"es6": true
+	},
+	"parserOptions": {
+		"ecmaVersion": 8
+	},
+	"extends": ["eslint:recommended", "google"],
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-negated-condition": "off",
+		"require-jsdoc": "off",
+		"no-mixed-requires": "off",
+		"max-len": ["warn", 120],
+		"no-implicit-coercion": [
+			2,
+			{"allow": ["!!"]}
+		],
+		"comma-dangle": "off",
+		"no-var": "off",
+		"no-tabs": "off",
+		"no-console": "off"
+	},
+	"root": true
 };
